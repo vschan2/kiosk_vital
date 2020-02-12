2db5c2ebca64487f79d2d49930357dc7334237c1
vschan2 <chanveisiang@hotmail.com>
2020-02-13 02:34:58 +0800

    'Initialization'

---
+++ Assets/Scripts/TimelineController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineController : MonoBehaviour
+{
+    public PlayableDirector playableDirector;
+    public GameObject imageTarget;
+
+    public void Play() 
+    {
+      playableDirector.Play();
+    }
+
+    public void Stop()
+    {
+      playableDirector.Stop();
+    }
+
+    public void Update()
+    {
+      // If the image target is detected, play the timeline. Else, stop the timeline.
+      if(imageTarget.GetComponent<DefaultTrackableEventHandler>().mTrackedObjFound)
+        Play();
+      else
+        Stop();
+    }
+}
