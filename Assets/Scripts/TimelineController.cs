22591ebcf5adfddc1ac04d73fa7f114c3e3f1c79
vschan2 <chanveisiang@hotmail.com>
2020-02-17 00:55:03 +0800

    Added AR interaction

--- Assets/Scripts/TimelineController.cs
+++ Assets/Scripts/TimelineController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
@@ -8,6 +8,9 @@ public class TimelineController : MonoBehaviour
 {
     public PlayableDirector playableDirector;
     public List<TimelineAsset> timelines;
+
+    public bool isTrent = false;
+    public bool isUltrasonic = false;
 
     public void Play() 
     {
@@ -19,18 +22,34 @@ public class TimelineController : MonoBehaviour
       playableDirector.Stop();
     }
 
-    public void PlayFromTimelines(int index) {
+    public void PlayFromTimelines(int index) 
+    {
       TimelineAsset selectedAsset;
 
       if(timelines.Count <= index)
       {
         selectedAsset = timelines[timelines.Count - 1];
-      } 
+      }
       else 
       {
-        selectedAsset = timelines[index];
+        if(isTrent && isUltrasonic)
+        {
+          selectedAsset = timelines[1];
+        } 
+        else
+          selectedAsset = timelines[0];
       }
 
       playableDirector.Play(selectedAsset);
+    }
+
+    public void setIsTrent(bool b) 
+    {
+      isTrent = b;
+    }
+
+    public void setIsUltrasonic(bool b) 
+    {
+      isUltrasonic = b;
     }
 }
